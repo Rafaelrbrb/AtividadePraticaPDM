921547104ee17f986c0b01f52865ae42da355b9d
Davimpc <101334489+Davimpc@users.noreply.github.com>
2025-06-15 23:52:41 -0300

    Update AdicionarAssinatura.js

--- AdicionarAssinatura.js
+++ AdicionarAssinatura.js
@@ -1,89 +1,93 @@
-import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
-import {db} from '../src/firebaseConnection';
-import {collection, addDoc} from 'firebase/firestore';
-import { useState } from "react";
+import { View, TextInput, Button, StyleSheet, Text, Alert } from "react-native";
+import { db } from "../src/firebaseConnection";
+import { collection, addDoc, Timestamp} from "firebase/firebase";
+import { useState, useContext } from "react";
+import { AuthContext } from "../src/auth-contexto";
 
-    export default function AdicionarAssinatura(){
-            const [nome, setNome] = useState('');
-            const [valorMensal, setValorMensal] = useState('');
-            const [dataRenovacao, setDataRenovacao] = useState('');
-            const [categoria, setCategoria] = useState('');
-        
-            const adicionarAssinatura = async () =>{
-                if(!nome || !valorMensal || !dataRenovacao || !categoria){
-                    Alert.alert('Preencha todos os campos');
-                    return;
-                }
-        
-                try{
-                    await addDoc(collection(db, 'uid'),{
-                        nome,
-                        valorMensal: parseFloat(valorMensal),
-                        dataRenovacao: data,
-                        categoria,
-                    });
-        
-                Alert.alert('Assinatura adicionada');
-                setNome('');
-                setValorMensal('');
-                setDataRenovacao('');
-                setCategoria('');
-                }catch(error){
-                    console.log('Erro ao adicionar assinatura: ', error);
-                }
-            }
+export default function AdicionarAssinatura() {
+  const [nome, setNome] = useState("");
+  const [valorMensal, setValorMensal] = useState("");
+  const [dataRenovacao, setDataRenovacao] = useState("");
+  const [categoria, setCategoria] = useState("");
 
-        return(
-            <View style = {styles.container}>
-                <Text style = {styles.text}> Assinatura </Text>
-                <TextInput 
-                 style = {styles.input} 
-                 value = {nome}
-                 onChangeText={setNome}
-                 />
+  const { uid } = useContext(AuthContext);
 
-                 <Text style = {styles.text}>Preço R$</Text>
-                 <TextInput
-                 style = {styles.input}
-                 value = {valorMensal}
-                 onChangeText = {setValorMensal}
-                 />
-
-                 <Text style = {styles.text}> Data da renovação </Text> {/*ARRUMAR*/}
-                 <TextInput
-                 style = {styles.input}
-                 value = {dataRenovacao}
-                 onChangeText = {setDataRenovacao}
-                 />
-
-                 <Text style = {styles.text}> Categoria </Text>
-                 <TextInput
-                 style = {styles.input}
-                 value = {categoria}
-                 onChangeText = {setCategoria}
-                 placeholder={"ex: Streaming, Educação, Software, Outros"}
-                 />
-
-                 <Button title = 'Adicionar assinatura' onPress = {AdicionarAssinatura}/>
-            </View>
-
-        )
+  const adicionarAssinatura = async () => {
+    id(!nome || !valorMensal || !dataRenovacao || !categoria) {
+      Alert.alert("Erro", "Preencha todos os campos");
+      return;
     }
 
-    const styles = StyleSheet.create({
-        container: {
-          padding: 16,
-          marginTop: 30,
-        },
-        text: {
-          marginBottom: 4,
-          fontWeight: 'bold',
-        },
-        input: {
-          borderWidth: 1,
-          borderColor: '#999',
-          padding: 8,
-          marginBottom: 12,
-          borderRadius: 4,
-        },
+    try {
+      await addDoc(collection(db, `usuarios/${uid}/assinaturas`), {
+        nome,
+        valor: parseFloat(valorMensal),
+        dataRenovacao: Timestamp.fromDate(new Date(dataRenovacao)),
+        categoria,
       });
+
+    Alert.alert("Sucesso", "Assinatura adicionada com sucesso");
+      setNome("");
+      setValorMensal("");
+      setDataRenovacao("");
+      setCategoria("");
+    } catch (error) {
+      console.log("Erro ao adicionar assinatura:", error);
+      Alert.alert("Erro", "Não foi possível adicionar a assinatura");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Assinatura</Text>
+      <TextInput style={styles.input} value={nome} onChangeText={setNome} />
+
+      <Text style={styles.text}>Preço R$</Text>
+      <TextInput
+        style={styles.input}
+        value={valorMensal}
+        onChangeText={setValorMensal}
+        keyboardType="numeric"
+      />
+
+      <Text style ={styles.text}>Data da Renovação</Text>
+      <TextInput
+        style{styles.input}
+        value={dataRenovacao}
+        placeholder="AAAA-MM-DD"
+        keyboardType="numeric"
+      />
+
+      <Text style={styles.text}>Categoria</Text>
+      <TextInput
+        styles={styles.input}
+        value={categoria}
+        onChangeText={setCategoria}
+        placeholder="ex: Streaming, Educação, Software, Outros"
+      />
+
+      <Button title="Adicionar assinatura" onPress={adicionarAssinatura} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    marginTop: 30,
+  },
+  text: {
+    marginBottom: 4,
+    fontWeight: "bold",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#999",
+    padding: 8,
+    marginBottom: 12,
+    borderRadius: 4,
+  },
+});
+    
+      
+    
