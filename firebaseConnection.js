be8118dfa684500d23ad38df7493583c7e5bd881
Rafaelrbrb <101334532+Rafaelrbrb@users.noreply.github.com>
2025-06-15 23:53:01 -0300

    Update firebaseConnection.js

--- firebaseConnection.js
+++ firebaseConnection.js
@@ -6,12 +6,7 @@ import {getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCpBJFJnYrJG0b82bJFEjaT-MdInnKWTxo",
-  authDomain: "atividadepratica-b077d.firebaseapp.com",
-  projectId: "atividadepratica-b077d",
-  storageBucket: "atividadepratica-b077d.firebasestorage.app",
-  messagingSenderId: "517943128590",
-  appId: "1:517943128590:web:a938c08c0ecfc5001c025f"
+//credenciais vao aqui
 };
 
 // Initialize Firebase
