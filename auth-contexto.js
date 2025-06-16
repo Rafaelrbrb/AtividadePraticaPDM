1db45c5f076e79e8b43c198238b0f6c39e52a6a9
Davimpc <101334489+Davimpc@users.noreply.github.com>
2025-06-15 22:53:35 -0300

    Add files via upload

---
+++ auth-contexto.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext({
+  uid: "1",
+});
+
+function AuthProvider({ children }) {
+  const [uid, setUid] = useState("1");
+
+  return (
+    <AuthContext.Provider value={{ uid, setUid }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
